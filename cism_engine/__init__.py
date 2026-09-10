2852fad6c7c5f71b4082cf9ec74caf5da9c391c7
tyquan8900 <tyquan8900@gmail.com>
2026-09-10 18:32:36 -0400

    Remove legacy package init

--- cism_engine/__init__.py
+++
@@ -1,6 +0,0 @@
-"""CISM Reasoning Engine."""
-
-from .models import Attempt, LearnerState, Question
-from .reasoning import evaluate, reveal, weakest_domain
-
-__all__ = ["Attempt", "LearnerState", "Question", "evaluate", "reveal", "weakest_domain"]
